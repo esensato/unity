23c435323ae9df2b476b1b231abe2856f83615c5
ealex <ealex@br.ibm.com>
2020-06-13 18:00:59 -0300

    Initial version

---
+++ TicTacToe/Assets/code/TileClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileClick : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Tile;
+
+
+    public void OnMouseDown() {
+
+            Debug.Log("Clicked: " + Tile.name);
+
+    }
+
+    void Start()
+    {
+        Debug.Log("Hello World.");
+    }
+
+    void Update()
+    {
+        
+    }
+}
